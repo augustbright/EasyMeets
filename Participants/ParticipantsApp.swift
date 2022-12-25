c80e6d3db38d422264896e2bf91d8d07a81db819
August Bright <augustbright@yandex.ru>
2022-12-25 20:20:01 +0400

    Initial Commit

---
+++ Participants/ParticipantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticipantsApp.swift
+//  Participants
+//
+//  Created by Валерий Бубенщиков on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ParticipantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
